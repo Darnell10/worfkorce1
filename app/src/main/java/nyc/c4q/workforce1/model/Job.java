7707df6e486daceff80e524f26ac235588414a55
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-03-03 12:22:08 -0500

    jobs model

--- app/src/main/java/nyc/c4q/workforce1/model/Job.java
+++ app/src/main/java/nyc/c4q/workforce1/model/Job.java
@@ -6,6 +6,89 @@ package nyc.c4q.workforce1.model;
 
 public class Job {
 
+    String boroughBronx;
+    String boroughBrooklyn;
+    String boroughManhattan;
+    String boroughNonNyc;
+    String boroughQueens;
+    String boroughStatenIsland;
+    String candidateExperienceQualificationSkills;
+    String educationRequired;
+    String hourlyAnnual;
+    int maxHoursPerWeek;
+    int minHoursPerWeek;
+    String positionDescritption;
+    String positionTitle;
+    String positionType;
+    String sectorName;
+    Double wageMax;
+    Double wageMin;
 
+    public String getBoroughBronx() {
+        return boroughBronx;
+    }
 
+    public String getBoroughBrooklyn() {
+        return boroughBrooklyn;
+    }
+
+    public String getBoroughManhattan() {
+        return boroughManhattan;
+    }
+
+    public String getBoroughNonNyc() {
+        return boroughNonNyc;
+    }
+
+    public String getBoroughQueens() {
+        return boroughQueens;
+    }
+
+    public String getBoroughStatenIsland() {
+        return boroughStatenIsland;
+    }
+
+    public String getCandidateExperienceQualificationSkills() {
+        return candidateExperienceQualificationSkills;
+    }
+
+    public String getEducationRequired() {
+        return educationRequired;
+    }
+
+    public String getHourlyAnnual() {
+        return hourlyAnnual;
+    }
+
+    public int getMaxHoursPerWeek() {
+        return maxHoursPerWeek;
+    }
+
+    public int getMinHoursPerWeek() {
+        return minHoursPerWeek;
+    }
+
+    public String getPositionDescritption() {
+        return positionDescritption;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public String getPositionType() {
+        return positionType;
+    }
+
+    public String getSectorName() {
+        return sectorName;
+    }
+
+    public Double getWageMax() {
+        return wageMax;
+    }
+
+    public Double getWageMin() {
+        return wageMin;
+    }
 }
