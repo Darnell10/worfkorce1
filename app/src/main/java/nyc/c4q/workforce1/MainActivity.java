049d477510549ad1650f145693e2148a60082c19
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-03-03 16:48:01 -0500

    commit

--- app/src/main/java/nyc/c4q/workforce1/MainActivity.java
+++ app/src/main/java/nyc/c4q/workforce1/MainActivity.java
@@ -1,10 +1,12 @@
 package nyc.c4q.workforce1;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,6 +28,8 @@ public class MainActivity extends FragmentActivity {
         pager = (ViewPager) findViewById(R.id.main_view_pager);
         pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
+
+
     }
 
     @Override
