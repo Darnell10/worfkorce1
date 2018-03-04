f05861039437db248dfbde6a136b8a4be3a094fb
Maria David <mhdavid83@yahoo.com>
2018-03-04 11:16:48 -0500

    updated with presenter for filter to test

---
+++ app/src/androidTest/java/nyc/c4q/workforce1/FilterPresenterTest.java
@@ -0,0 +1,56 @@
+package nyc.c4q.workforce1;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import nyc.c4q.workforce1.model.Job;
+import nyc.c4q.workforce1.presenter.FilterPresenter;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by C4Q on 3/4/18.
+ */
+
+public class FilterPresenterTest {
+
+    @Mock
+    Job mockJob;
+
+    @Mock
+
+    FilterPresenter filterPresenter;
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        // configure our mocks... remember, mock methods are just stubs:
+        // unless we tell the mock what to return when the stub is called, it will just return null.
+        // let's return the mockData object instead, so we can verify that the return value of api.getData()
+        //is really what we're storing in the database.
+
+//        when(mockApi.getData()).thenReturn(mockData);
+//
+//        myPresenter = new MyPresenter(mockDatabase, mockApi);
+    }
+
+
+    @Test
+    public void fetchAndStoreData_storesApiResponseInDatabase() {
+
+        // first, call the method we want to test
+//        myPresenter.fetchAndStoreData();
+//
+//        // next, verify that the expected actions were invoked on our mock dependencies
+//        verify(mockApi).getData();
+//
+//        // remember, since our mockApi is returning mockData, we want to verify that this is exactly what we're storing
+//        verify(mockDatabase).addData(mockData);
+
+        // if this test passes, then fetchAndStoreData() is behaving as expected!
+    }
+}
---
+++ app/src/main/java/nyc/c4q/workforce1/presenter/FilterPresenter.java
@@ -0,0 +1,57 @@
+package nyc.c4q.workforce1.presenter;
+
+import android.util.Log;
+
+import java.util.List;
+
+import nyc.c4q.workforce1.FilterDummyData;
+import nyc.c4q.workforce1.model.DummyData;
+import nyc.c4q.workforce1.model.Event;
+import nyc.c4q.workforce1.model.Job;
+
+/**
+ * Created by C4Q on 3/4/18.
+ */
+
+public class FilterPresenter {
+
+    // a reference to your Lists. assume it has a method something like: database.addData(Data data)
+    public static List<Job> jobList = DummyData.jobList;
+    public static List<Event> eventList = DummyData.eventList;
+    public List<Job> filteredjobs;
+    public List<Event> filteredevents;
+
+    // a reference to your retrofit api or networking assemble. assume it has a method something like: api.getData()
+    // private RetrofitService api;
+
+    public FilterPresenter(List<Job> jobList, List<Event> eventList) {
+        this.jobList = jobList;
+        this.eventList = eventList;
+    }
+
+    public void filterData() {
+
+        FilterDummyData filterCriteria = new FilterDummyData();
+
+        filterCriteria.setPositionType("Film & Industry");
+        filterCriteria.setWageMax(24.00);
+        filterCriteria.setWageMin(15.00);
+
+        for (int i = 0; i < jobList.size(); i++) {
+            for (int j = 0; j < eventList.size(); j++) {
+                Job job = jobList.get(i);
+                Event event = eventList.get(j);
+                Log.d("EVENTS: ", "onCreate: " + event);
+                Log.d("JOBS: ", "onCreate: " + job);
+                if (job.getWageMin() >= filterCriteria.getWageMin()
+                        && job.getWageMax() < filterCriteria.getWageMax()
+                        && job.getPositionType() == filterCriteria.getPositionType()) {
+                    filteredjobs.add(job);
+                    filteredevents.add(event);
+                }
+            }
+        }
+
+    }
+
+}
