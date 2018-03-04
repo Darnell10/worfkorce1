91a9016e4db3acdf6be931bd13661e65adc197c3
Maria David <mhdavid83@yahoo.com>
2018-03-04 01:58:51 -0500

    updated filter of jobs and events

--- app/src/main/java/nyc/c4q/workforce1/FilterActivity.java
+++ app/src/main/java/nyc/c4q/workforce1/FilterActivity.java
@@ -3,21 +3,17 @@ package nyc.c4q.workforce1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.List;
-
 import nyc.c4q.workforce1.model.DummyData;
 import nyc.c4q.workforce1.model.Event;
 import nyc.c4q.workforce1.model.Job;
-
-import static nyc.c4q.workforce1.model.DummyData.populateEventList;
-import static nyc.c4q.workforce1.model.DummyData.populateJobList;
 
 public class FilterActivity extends AppCompatActivity {
 
     public static List<Job> jobList = DummyData.jobList;
     public static List<Event> eventList = DummyData.eventList;
     public List<Job> filteredjobs;
+    public List<Event> filteredevents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +27,20 @@ public class FilterActivity extends AppCompatActivity {
         filterCriteria.setWageMin(15.00);
 
         for (int i = 0; i < jobList.size(); i++) {
-            Job job = jobList.get(i);
-            Log.d("Position", "onCreate: " + job);
-            if (job.getWageMin() >= filterCriteria.getWageMin()
-                    && job.getWageMax() < filterCriteria.getWageMax()
-                    && job.getPositionType() == filterCriteria.getPositionType()) {
-                filteredjobs.add(job);
+            for (int j = 0; j < eventList.size(); j++) {
+                Job job = jobList.get(i);
+                Event event = eventList.get(j);
+                Log.d("EVENTS: ", "onCreate: " + event);
+                Log.d("JOBS: ", "onCreate: " + job);
+                if (job.getWageMin() >= filterCriteria.getWageMin()
+                        && job.getWageMax() < filterCriteria.getWageMax()
+                        && job.getPositionType() == filterCriteria.getPositionType()) {
+                    filteredjobs.add(job);
+                    filteredevents.add(event);
+                }
             }
+
         }
+
     }
 }
