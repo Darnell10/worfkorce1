bc08e81cff237a69cd0d04d74a12c1faaac381f4
Amy Rivera <amyrivera.ar@gmail.com>
2018-03-04 10:33:36 -0500

    Event detail activity fixes

--- app/src/main/java/nyc/c4q/workforce1/fragments/EventsDetailActivity.java
+++ app/src/main/java/nyc/c4q/workforce1/fragments/EventsDetailActivity.java
@@ -12,9 +12,8 @@ import nyc.c4q.workforce1.model.Event;
 
 public class EventsDetailActivity extends AppCompatActivity {
     TextView borough, checkInFrom, checkInTo, companyName, eventDate, eventTitle,location,locationNameAndAddress,qualifications;
+
     Event event = DummyData.eventList.get(0);
-
-
 
 
     @Override
@@ -22,37 +21,31 @@ public class EventsDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_detail);
 
-//        borough= findViewById(R.id.job_borough);
-//        checkInFrom =findViewById(R.id.checkIn_From);
-//        checkInTo =findViewById(R.id.event_title);
-//        companyName =findViewById(R.id.company_Name);
-//        eventDate =findViewById(R.id.date);
-//        eventTitle =findViewById(R.id.event_title);
-//        location =findViewById(R.id.job_location);
-//        locationNameAndAddress =findViewById(R.id.location_address);
-//        qualifications =findViewById(R.id.qualification);
+        borough= findViewById(R.id.event_borough);
+        checkInFrom =findViewById(R.id.checkIn_From);
+        checkInTo =findViewById(R.id.event_title);
+        companyName =findViewById(R.id.event_company_Name);
+        eventDate =findViewById(R.id.event_date);
+        eventTitle =findViewById(R.id.event_title);
+        location =findViewById(R.id.job_location);
+        locationNameAndAddress =findViewById(R.id.event_location_address);
+        qualifications =findViewById(R.id.event_qualification);
+
+        borough.setText(event.getBorough());
+        checkInFrom.setText(event.getCheckInFrom().toString());
+        checkInTo.setText(event.getCheckInTo().toString());
+        companyName.setText(event.getCompanyNameOrType());
+        eventDate.setText(event.getEventDate().toString());
+        eventTitle.setText(event.getEventTitle());
+        location.setText(event.getLocation());
+        locationNameAndAddress.setText(event.getLocationNameAndAddress());
+        qualifications.setText(event.getQualifications());
+
 
     }
 }
 
 
-//            android:id="@+id/event_title"
-
-//            android:id="@+id/company_Name"
-
-//            android:id="@+id/date"
-
-//            android:id="@+id/job_borough"
-
-//            android:id="@+id/job_location"
-
-//            android:id="@+id/location_address"
-
-//            android:id="@+id/checkIn_From"
-
-//            android:id="@+id/checkIn_To"
-
-//            android:id="@+id/qualification"
 
 
 
