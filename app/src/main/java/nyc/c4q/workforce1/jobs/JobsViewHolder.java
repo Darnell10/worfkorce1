8958ae10d82d956e1d69ac0ddfa8c6788562a362
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-03-04 11:24:46 -0500

    added intent to jobs detail activity

--- app/src/main/java/nyc/c4q/workforce1/jobs/JobsViewHolder.java
+++ app/src/main/java/nyc/c4q/workforce1/jobs/JobsViewHolder.java
@@ -1,10 +1,12 @@
 package nyc.c4q.workforce1.jobs;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import nyc.c4q.workforce1.R;
+import nyc.c4q.workforce1.fragments.JobsDetailActivity;
 import nyc.c4q.workforce1.model.Job;
 
 /**
@@ -30,9 +32,41 @@ public class JobsViewHolder extends RecyclerView.ViewHolder {
 
     }
     public void onBind(Job job) {
+        final String title = job.getPositionTitle();
+        final String type = job.getPositionType();
+        final String boro = job.getBorough();
+        final String desc = job.getPositionDescritption();
+        final String maxW = job.getWageMax().toString();
+        final String minW = job.getWageMin().toString();
+        final String maxH = Integer.toString(job.getMaxHoursPerWeek());
+        final String minH = Integer.toString(job.getMinHoursPerWeek());
+        final String exp = job.getCandidateExperienceQualificationSkills();
+        final String req = job.getEducationRequired();
+
+
+
         tv1.setText(job.getPositionTitle());
         tv2.setText(job.getPositionType());
         tv3.setText(job.getBorough());
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotToDetail = new Intent(itemView.getContext(), JobsDetailActivity.class);
+                gotToDetail.putExtra("title",title);
+                gotToDetail.putExtra("type",type);
+                gotToDetail.putExtra("borough",boro);
+                gotToDetail.putExtra("desc",desc);
+                gotToDetail.putExtra("wageMax",maxW);
+                gotToDetail.putExtra("wageMin",minW);
+                gotToDetail.putExtra("hoursMax",maxH);
+                gotToDetail.putExtra("hoursMin",minH);
+                gotToDetail.putExtra("exp",exp);
+                gotToDetail.putExtra("req",req);
+                itemView.getContext().startActivity(gotToDetail);
+
+            }
+        });
 
     }
 }
